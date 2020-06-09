ae951a7b37da0144457ac08d6d808676f0ee330a
jubuyer <jahmed10@stuy.edu>
2020-06-08 22:56:15 -0400

    Simple key w/the gif, Player to interact with it

---
+++ Key.java
@@ -0,0 +1,12 @@
+package info.gridworld.actor;
+
+import info.gridworld.grid.Grid;
+import info.gridworld.grid.Location;
+
+import java.awt.Color;
+
+public class Key extends Actor {
+  public Key () {
+    setColor(null);
+  }
+}

dd2495123d5003aa2fa47a2e16aad99201f9f689
jubuyer <jahmed10@stuy.edu>
2020-06-10 13:10:59 -0400

    New skull class to be dropped when Player dies

---
+++ info/gridworld/actor/Skull.java
@@ -0,0 +1,15 @@
+package info.gridworld.actor;
+
+ import info.gridworld.grid.Grid;
+ import info.gridworld.grid.Location;
+
+ import java.awt.Color;
+
+ public class Skull extends Actor
+ {
+     public Skull()
+     {
+       setColor(null);
+     }
+
+ }

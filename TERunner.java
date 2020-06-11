b2d2680230bd95e4aca5359dae74a1c545a30cba
Imperatoras <hwilliams10@stuy.edu>
2020-06-10 22:13:06 -0400

    Story added

--- TERunner.java
+++ TERunner.java
@@ -32,6 +32,15 @@
 
      public static void main(String[] args)
      {
+         System.out.println("\tTOMB ESCAPE - By Helena Williams, Diya Mohan, and Jubayer Ahmed");
+         System.out.println("\tSTORY:");
+         System.out.println("\pYou\'ve finally done it. After months of searching, you\'ve found the lost tomb of the great pharaoh Akhmenad the Fifth! You carefully pry the slab covering the entrance away with your crowbar and turn on your flashlight. The hallway beyond is long, made of sandstone bricks, and covered in ancient hieroglyphs, which are eroded and illegible. Torches, which have long since burnt out, are mounted every few feet.");
+         System.out.println("You carefully enter the tomb and begin to walk down the hallway, only to startle in fright when you see two eyes shimmering in the darkness! Yet when you turn the beam of your flashlight onto it, it\'s only the gemstones embedded into the eye sockets of a sphinx, which are a beautiful blue color. You briefly consider prying the eyes out - after all, they\'d sell for a fortune - but something about it doesn\'t feel right.");
+         System.out.println("Sphinx encounter over, you begin to search the tomb. Its dusty halls are filled with mummies - most likely the servants of the pharaoh, embalmed so that they would serve him in the afterlife. Some of them have fallen from their plinths and lie on the ground in contorted positions. Yet strangely enough, they appear to be wearing the uniforms of British army officers from the 19th century. Why would they be in a tomb like this?");
+         System.out.println("You brush aside your concerns and continue deeper into the tomb. And after a little more searching, you find something incredible - a statuette of the god Anubis! The statuette is in pristine condition, and ornamented with gold and jewels. Eagerly, you reach out and delicately lift the statuette from its plinth, excited to present it to the world.");
+         System.out.println("Yet the second you do, a great flash of red light causes you to cover your eyes. The sandstone floor rumbles beneath your feet. And suddenly, the tomb\'s hieroglyphs begin to glow a bright red.");
+         System.out.println("And with that, those mummies are picking themselves off the floor and eyeing you menacingly. The sphinxes rise as well, and you realize that the gemstones in their eyes are now glowing red. One of them opens its mouth, and suddenly the air begins to heat up. A fireball comes roaring out of the sphinx\'s throat, and you fling yourself to the ground as it passes above you.");
+         System.out.println("It\'s clear now that you\'re in dire straits. If you don\'t escape from this tomb, and fast, you\'ll be joining the mummies!");
          ActorWorld world = new ActorWorld();
          for(int x = 0; x < 15; x++){ //For some reason, the x and y are swapped. DON'T ASK.
            for(int y = 0; y < 15; y++){
